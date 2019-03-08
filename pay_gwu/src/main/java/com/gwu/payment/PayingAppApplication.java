a10b4250b5fff84b3c650951c05286473d44b497
gwu <772908980@qq.com>
2019-03-08 17:06:42 +0800

    增加包解决io.nety.Tim出错

--- pay_gwu/src/main/java/com/gwu/payment/PayingAppApplication.java
+++ pay_gwu/src/main/java/com/gwu/payment/PayingAppApplication.java
@@ -1,32 +1,31 @@
-//package com.gwu.payment;
-//
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-//import org.springframework.boot.builder.SpringApplicationBuilder;
-//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-//import org.springframework.scheduling.annotation.EnableScheduling;
-//import com.gwu.payment.wechat.util.TokenThread;
-///**启动mybatise事务*/
-////@MapperScan("com.ringbet.rabbit.database.mapper")
-////@EnableScheduling
-//@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-//public class PayingAppApplication  extends SpringBootServletInitializer implements CommandLineRunner  {
-//	public static void main(String[] args) {
-//		SpringApplication.run(PayingAppApplication.class, args);
-//	}
-//	
-//	@Override
-//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-//		builder.sources(this.getClass());
-//		return super.configure(builder);
-//	
-//	}
-//
-//	@Override
-//	public void run(String... args) throws Exception {
-//		new Thread(new TokenThread()).start();
-//	}
-//	
-//}
+package com.gwu.payment;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import com.gwu.payment.wechat.util.TokenThread;
+/**启动mybatise事务*/
+//@MapperScan("com.ringbet.rabbit.database.mapper")
+//@EnableScheduling
+@SpringBootApplication
+public class PayingAppApplication  extends SpringBootServletInitializer implements CommandLineRunner  {
+	public static void main(String[] args) {
+		SpringApplication.run(PayingAppApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		builder.sources(this.getClass());
+		return super.configure(builder);
+	
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		new Thread(new TokenThread()).start();
+	}
+	
+}
